68098b011ec2edab740d16e44e4da2ad67ab836a
irvanalfi <irvanhidayat0623@gmail.com>
2020-05-15 14:06:17 +0700

    rout
    Co-Authored-By: AlanPerdhanaTimor <alanperdhanatimor@users.noreply.github.com>

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------------
@@ -58,5 +58,22 @@ $route['translate_uri_dashes'] = TRUE;
 | Sample REST API Routes
 | -------------------------------------------------------------------------
 */
+
+// $route['customer'] = 'pelanggan';
+// $route['customer/add'] = 'pelanggan/add';
+// $route['customer/process'] = 'pelanggan/process';
+// $route['customer/edit/(:num)'] = 'pelanggan/edit/$1';
+// $route['customer/del/(:num)'] = 'pelanggan/del/$1';
+
+$route['stock/in'] = 'stock/stock_in_data';
+$route['stock/in/add'] = 'stock/stock_in_add';
+$route['stock/in/process'] = 'stock/stock_in_process';
+$route['stock/in/del/(:num)/(:num)'] = 'stock/stock_in_del';
+
+$route['stock/out'] = 'stock/stock_out_data';
+$route['stock/out/add'] = 'stock/stock_out_add';
+$route['stock/out/process'] = 'stock/stock_out_process';
+$route['stock/out/del/(:num)/(:num)'] = 'stock/stock_out_del';
+
 $route['api/example/users/(:num)'] = 'api/example/users/id/$1'; // Example 4
 $route['api/example/users/(:num)(\.)([a-zA-Z0-9_-]+)(.*)'] = 'api/example/users/id/$1/format/$3$4'; // Example 8
