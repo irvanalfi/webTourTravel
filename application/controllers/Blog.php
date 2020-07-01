12ed63634d4c6c5a05015b78b8dc1d03240f9121
gopla <yudhistn@gmail.com>
2020-07-01 19:25:24 +0700

    fix(app/blog): Fix image when on null

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -4,127 +4,127 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Blog extends CI_Controller
 {
 
-    public function __construct()
-    {
-        parent::__construct();
-        check_not_login();
-        $this->load->model('Blog_m');
-        $this->load->model('Cetak_m');
-    }
+  public function __construct()
+  {
+    parent::__construct();
+    check_not_login();
+    $this->load->model('Blog_m');
+    $this->load->model('Cetak_m');
+  }
 
-    public function index()
-    {
-        $data['row'] = $this->Blog_m->get();
-        $this->template->load('template', 'blog/blog_data', $data);
-    }
+  public function index()
+  {
+    $data['row'] = $this->Blog_m->get();
+    $this->template->load('template', 'blog/blog_data', $data);
+  }
 
-    public function proses()
-    {
-        $config['upload_path']      = './assets/img/blog/';
-        $config['allowed_types']    = 'gif|jpg|jpeg|png';
-        $config['max_size']         = 5120;
-        $config['file_name']        = 'blog-' . date('dmy') . '-' . substr(md5(rand()), 0, 10);
-        $this->load->library('upload', $config);
+  public function proses()
+  {
+    $config['upload_path']      = './assets/img/blog/';
+    $config['allowed_types']    = 'gif|jpg|jpeg|png';
+    $config['max_size']         = 5120;
+    $config['file_name']        = 'blog-' . date('dmy') . '-' . substr(md5(rand()), 0, 10);
+    $this->load->library('upload', $config);
 
-        $post = $this->input->post(null, TRUE);
+    $post = $this->input->post(null, TRUE);
 
-        if (isset($_POST['add'])) {
-            if (@$_FILES['image']['name'] != null) {
-                if ($this->upload->do_upload('image')) {
-                    $post['image']  =   $this->upload->data('file_name');
-                    $this->Blog_m->add($post);
-                    if ($this->db->affected_rows() > 0) {
-                        $this->session->set_flashdata('success', 'Data has been successfully saved!!');
-                    }
-                    redirect('blog');
-                } else {
-                    $error = $this->upload->display_errors();
-                    $this->session->set_flashdata('error', $error);
-                    redirect('Blog/add');
-                }
-            } else {
-                $post['image']  = null;
-                $this->Blog_m->add($post);
-                if ($this->db->affected_rows() > 0) {
-                    $this->session->set_flashdata('success', 'Data has been successfully saved!!');
-                }
-                redirect('blog');
-            }
-        } else if (isset($_POST['edit'])) {
-            if (@$_FILES['image']['name'] != null) {
-                if ($this->upload->do_upload('image')) {
-                    $blog = $this->Blog_m->get($post['id'])->row();
-                    if ($blog->image != null) {
-                        $target_file = './assets/img/blog/' . $blog->image;
-                        unlink($target_file);
-                    }
-                    $post['image']  =   $this->upload->data('file_name');
-                    $this->Blog_m->edit($post);
-                    if ($this->db->affected_rows() > 0) {
-                        $this->session->set_flashdata('success', 'Data has been successfully saved!!');
-                    }
-                    redirect('blog');
-                } else {
-                    $error = $this->upload->display_errors();
-                    $this->session->set_flashdata('error', $error);
-                    redirect('blog/add');
-                }
-            } else {
-                $post['image']  = null;
-                $this->Item_m->edit($post);
-                if ($this->db->affected_rows() > 0) {
-                    $this->session->set_flashdata('success', 'Data has been successfully saved!!');
-                }
-                redirect('blog');
-            }
-        }
-    }
-
-    public function add()
-    {
-        $blog = new stdClass();
-        $blog->blog_id = null;
-        $blog->title = null;
-        $blog->image = null;
-        $blog->content = null;
-        $blog->writer = null;
-        $data = [
-            'page' => 'add',
-            'row' => $blog
-        ];
-        $this->template->load('template', 'blog/blog_form', $data);
-    }
-
-    public function edit($id)
-    {
-        $query = $this->Blog_m->get($id);
-        if ($query->num_rows() > 0) {
-            $blog = $query->row();
-            $data = [
-                'page' => 'edit',
-                'row' => $blog
-            ];
-            $this->template->load('template', 'blog/blog_form', $data);
+    if (isset($_POST['add'])) {
+      if (@$_FILES['image']['name'] != null) {
+        if ($this->upload->do_upload('image')) {
+          $post['image']  =   $this->upload->data('file_name');
+          $this->Blog_m->add($post);
+          if ($this->db->affected_rows() > 0) {
+            $this->session->set_flashdata('success', 'Data has been successfully saved!!');
+          }
+          redirect('blog');
         } else {
-            echo "<script>alert('Data tidak ditemukan');";
-            echo "window.location='" . site_url('blog') . "'</script>";
+          $error = $this->upload->display_errors();
+          $this->session->set_flashdata('error', $error);
+          redirect('Blog/add');
         }
-    }
-
-    public function delete()
-    {
-        $id = $this->input->post('blog_id');
-        $blog = $this->Blog_m->get($id)->row();
-        if ($blog->image != null) {
+      } else {
+        $post['image']  = null;
+        $this->Blog_m->add($post);
+        if ($this->db->affected_rows() > 0) {
+          $this->session->set_flashdata('success', 'Data has been successfully saved!!');
+        }
+        redirect('blog');
+      }
+    } else if (isset($_POST['edit'])) {
+      if (@$_FILES['image']['name'] != null) {
+        if ($this->upload->do_upload('image')) {
+          $blog = $this->Blog_m->get($post['id'])->row();
+          if ($blog->image != null) {
             $target_file = './assets/img/blog/' . $blog->image;
             unlink($target_file);
+          }
+          $post['image']  =   $this->upload->data('file_name');
+          $this->Blog_m->edit($post);
+          if ($this->db->affected_rows() > 0) {
+            $this->session->set_flashdata('success', 'Data has been successfully saved!!');
+          }
+          redirect('blog');
+        } else {
+          $error = $this->upload->display_errors();
+          $this->session->set_flashdata('error', $error);
+          redirect('blog/add');
         }
-
-        $this->Blog_m->del($id);
-
+      } else {
+        $post['image'] = $post['oldImage'];
+        $this->Blog_m->edit($post);
         if ($this->db->affected_rows() > 0) {
-            echo "<script>alert('Data Berhasil di Hapus')</script>";
+          $this->session->set_flashdata('success', 'Data has been successfully saved!!');
         }
-        echo "<script>window.location='" . site_url('blog') . "'</script>";
+        redirect('blog');
+      }
     }
+  }
+
+  public function add()
+  {
+    $blog = new stdClass();
+    $blog->blog_id = null;
+    $blog->title = null;
+    $blog->image = null;
+    $blog->content = null;
+    $blog->writer = null;
+    $data = [
+      'page' => 'add',
+      'row' => $blog
+    ];
+    $this->template->load('template', 'blog/blog_form', $data);
+  }
+
+  public function edit($id)
+  {
+    $query = $this->Blog_m->get($id);
+    if ($query->num_rows() > 0) {
+      $blog = $query->row();
+      $data = [
+        'page' => 'edit',
+        'row' => $blog
+      ];
+      $this->template->load('template', 'blog/blog_form', $data);
+    } else {
+      echo "<script>alert('Data tidak ditemukan');";
+      echo "window.location='" . site_url('blog') . "'</script>";
+    }
+  }
+
+  public function delete()
+  {
+    $id = $this->input->post('blog_id');
+    $blog = $this->Blog_m->get($id)->row();
+    if ($blog->image != null) {
+      $target_file = './assets/img/blog/' . $blog->image;
+      unlink($target_file);
+    }
+
+    $this->Blog_m->del($id);
+
+    if ($this->db->affected_rows() > 0) {
+      echo "<script>alert('Data Berhasil di Hapus')</script>";
+    }
+    echo "<script>window.location='" . site_url('blog') . "'</script>";
+  }
 }
--- application/views/blog/blog_form.php
+++ application/views/blog/blog_form.php
@@ -1,61 +1,62 @@
 <section class="content-header">
-    <h1>Blog
-        <small>Table</small>
-    </h1>
-    <ol class="breadcrumb">
-        <li><a href=""><i class="fa fa-dashboard"></i></a></li>
-        <li class="active">Blog Table</li>
-    </ol>
+  <h1>Blog
+    <small>Table</small>
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href=""><i class="fa fa-dashboard"></i></a></li>
+    <li class="active">Blog Table</li>
+  </ol>
 </section>
 
 <!-- Main content -->
 <section class="content">
-    <div class="box">
-        <div class="box-header">
-            <h3 class="box-title"><?= ucfirst($page); ?> Blog</h3>
-            <div class="pull-right">
-                <a href="<?= site_url('blog'); ?>" class="btn btn-warning">
-                    <i class="fa fa-arrow-left"></i> Back</a>
-            </div>
-        </div>
-
-        <div class="box-body table-responsive">
-            <div class="row">
-                <div class="col-md-6">
-                    <?php echo form_open_multipart('blog/proses') ?>
-                    <div class="form-group">
-                        <label for="">Blog Title *</label>
-                        <input type="hidden" name="id" value="<?= $row->blog_id; ?>">
-                        <input type="text" name="title" class="form-control" value="<?= $row->title; ?>" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="">Image</label>
-                        <?php if ($page == 'edit') {
-                            if ($row->image != null) { ?>
-                                <div style="margin-bottom:5px">
-                                    <img src="<?= base_url('assets/img/blog/' . $row->image) ?>" style="width:50%">
-                                </div>
-                        <?php
-                            }
-                        } ?>
-                        <input type="file" name="image" class="form-control">
-                        <small>(Leave blank if not <?= $page == 'edit' ? 'change' : 'available' ?>)</small>
-                    </div>
-                    <div class="form-group">
-                        <label for="">Blog Content *</label>
-                        <textarea name="content" id="" class="form-control" required><?= $row->content; ?></textarea>
-                    </div>
-                    <div class="form-group">
-                        <label for="">Blog Writer *</label>
-                        <input type="text" name="writer" class="form-control" value="<?= $row->writer; ?>" required>
-                    </div>
-                    <div class="from-group">
-                        <button type="submit" name="<?= $page; ?>" class="btn btn-success"><i class="fa fa-paper-plane"></i> Save</button>
-                        <button type="reset" class="btn" style="margin-left: 10px"><i class="fa fa-undo"></i> Reset</button>
-                    </div>
-                    <?php echo form_close() ?>
-                </div>
-            </div>
-        </div>
+  <div class="box">
+    <div class="box-header">
+      <h3 class="box-title"><?= ucfirst($page); ?> Blog</h3>
+      <div class="pull-right">
+        <a href="<?= site_url('blog'); ?>" class="btn btn-warning">
+          <i class="fa fa-arrow-left"></i> Back</a>
+      </div>
     </div>
+
+    <div class="box-body table-responsive">
+      <div class="row">
+        <div class="col-md-6">
+          <?php echo form_open_multipart('blog/proses') ?>
+          <div class="form-group">
+            <label for="">Blog Title *</label>
+            <input type="hidden" name="id" value="<?= $row->blog_id; ?>">
+            <input type="text" name="title" class="form-control" value="<?= $row->title; ?>" required>
+          </div>
+          <div class="form-group">
+            <label for="">Image</label>
+            <?php if ($page == 'edit') {
+              if ($row->image != null) { ?>
+                <div style="margin-bottom:5px">
+                  <img src="<?= base_url('assets/img/blog/' . $row->image) ?>" style="width:50%">
+                  <input type="hidden" name="oldImage" value="<?= $row->image ?>">
+                </div>
+            <?php
+              }
+            } ?>
+            <input type="file" name="image" class="form-control">
+            <small>(Leave blank if not <?= $page == 'edit' ? 'change' : 'available' ?>)</small>
+          </div>
+          <div class="form-group">
+            <label for="">Blog Content *</label>
+            <textarea name="content" id="" class="form-control" required><?= $row->content; ?></textarea>
+          </div>
+          <div class="form-group">
+            <label for="">Blog Writer *</label>
+            <input type="text" name="writer" class="form-control" value="<?= $row->writer; ?>" required>
+          </div>
+          <div class="from-group">
+            <button type="submit" name="<?= $page; ?>" class="btn btn-success"><i class="fa fa-paper-plane"></i> Save</button>
+            <button type="reset" class="btn" style="margin-left: 10px"><i class="fa fa-undo"></i> Reset</button>
+          </div>
+          <?php echo form_close() ?>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
