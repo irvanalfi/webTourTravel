d7391c0a20e684d8adb6fb966c39daa060f23783
irvanalfi <irvanhidayat0623@gmail.com>
2020-11-27 15:18:20 +0700

    perbaikan tampilan

--- application/views/client/privacy_policy.php
+++ application/views/client/privacy_policy.php
@@ -6,7 +6,7 @@
     </div>
 </section>
 </div>
-<section style="margin-top:-230px; margin-bottom:80px;">
+<section style="margin-top:-230px; margin-bottom:80px; font-family: Montserrat !important;">
     <div class="container">
         <div class="sections">
             <div class="section-title">
@@ -16,56 +16,77 @@
                         <b>Privacy Policy</b> </div>
                 </div>
             </div>
-            <!-- <div class="section-subtitle">
-                Find your own favorite destination. Let’s get it!</div> -->
         </div>
     </div>
 </section>
-<div class="works_area">
+<section style="font-family: Montserrat !important;">
     <div class="container">
-        <div class="row">
+        <hr style="margin-top:30px ;">
+        <div class="row mb-5">
             <div class="col-lg-12">
                 <div class="section_title text-center mb-90">
                     <span class="wow lightSpeedIn" data-wow-duration="1s" data-wow-delay=".1s"></span>
                 </div>
             </div>
         </div>
-        <div class="row">
+        <div class="row mb-5">
             <div class="col-md-6 col-lg-6">
-                <div class="single_works wow fadeInUp">
-                <h2 class="pt-4 mb-3" style="color: #0098c7;"><b>Privacy Policy Coverage</b></h2>
-                    <p>The Privacy Policy covers our treatment of personally identifiable information that we collect when you are on the Traviora.com site, and when you use our services.
-
-                        When using the Traviora.com site, you may also use the services offered by other parties. In particular, payment systems are provided by third parties and the information you provide to facilitate payment are collected by such third parties and not us.
-
-                        This Privacy Policy does not apply to the practices of companies that we do not own or control or to people that we do not employ or manage. Please check the privacy practices of these other companies or persons (if any) to understand how they treat the personally identifiable information that you provide to them and contact them if you have any concerns.</p>
+                <div class="single_works wow fadeInUp pr-5">
+                    <h1 class="mt-2 mb-5" style="color: #0098c7;">Privacy Policy Coverage</h1>
+                    <h4 class="mb-3" style="text-align: justify;">
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        The Privacy Policy covers our treatment of personally identifiable information that we
+                        collect when you are on the Traviora.com site, and when you use our services.
+                        When using the Traviora.com site, you may also use the services offered by other parties.
+                        In particular, payment systems are provided by third parties and the information you provide
+                        to facilitate payment are collected by such third parties and not us.
+                    </h4>
+                    <h4 class="mb-3" style="text-align: justify;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        This Privacy Policy does not apply to the practices of companies that we do not own or
+                        control or to people that we do not employ or manage. Please check the privacy practices of
+                        these other companies or persons (if any) to understand how they treat the personally identifiable
+                        information that you provide to them and contact them if you have any concerns.</h4>
                 </div>
             </div>
             <div class="col-md-6 col-lg-6">
-                <div class="single_works wow fadeInUp">
-                    <h2 class="pt-4 mb-3 primary" style="color: #0098c7;"><b>Information Sharing and Disclosure by us</b></h2>
-                    <p>We will not sell or rent your personally identifiable information to anyone. We will send personally identifiable information about you to other companies or people when:</p>
-                    <p>1. We have your consent to share the information;</p>
-                    <p>2. We need to share your information to provide the product or service you have requested;</p>
-                    <p>3. We need to send the information to companies who work on behalf of us to provide a product or service to you. (Unless we tell you differently, these companies do not have any right to use the personally identifiable information we provide to them beyond what is necessary to assist us.);</p>
-                    <p>4. We respond to subpoenas, court orders or legal process; or</p>
-                    <p>5. We find that your actions on our web sites violate our Terms of Service, or any of our usage guidelines for specific products or services.</p>
+                <div class="single_works wow fadeInUp pl-5">
+                    <h1 class="mt-2 mb-5" style="color: #0098c7;">Information Sharing and Disclosure by us</h1>
+                    <h4 class="mb-3" style="text-align: justify;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        We will not sell or rent your personally identifiable information to anyone.
+                        We will send personally identifiable information about you to other companies or people when:
+                    </h4>
+                    <h4 class="mb-3" style="text-align: justify;">
+                        <ol>
+                            <li>We have your consent to share the information.</li>
+                            <li>We need to share your information to provide the product or service you have requested.</li>
+                            <li>We need to send the information to companies who work on behalf of us to provide a product or service to you. (Unless we tell you differently, these companies do not have any right to use the personally identifiable information we provide to them beyond what is necessary to assist us).</li>
+                            <li>We respond to subpoenas, court orders or legal process. or</li>
+                            <li>We find that your actions on our web sites violate our Terms of Service, or any of our usage guidelines for specific products or services.</li>
+                        </ol>
                 </div>
             </div>
         </div>
-        <div class="row">
+        <div class="row mb-5">
             <div class="col-md-6 col-lg-6">
-                <div class="single_works wow fadeInUp">
-                <h2 class="pt-4 mb-3 " style="color: #0098c7;"><b>Information Collection and Use by Traviora.com</b></h2>
-                    <p>We collect personally identifiable information when you register for our service. We automatically receive and record information on our server logs from your browser including your IP address, the page you requested, and the personally identifiable information you entered.</p>
-                    <p>We use information for three general purposes: to customize the advertising and content you see, to fulfill your requests for certain products and services, and to contact you in case of emergency.</p>
+                <div class="single_works wow fadeInUp pr-5">
+                    <h1 class="mt-4 mb-5 " style="color: #0098c7;">Information Collection and Use by Traviora.com</h1>
+                    <h4 class="mb-3" style="text-align: justify;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        We collect personally identifiable information when you register for our service. We automatically receive and record information on our server logs from your browser including your IP address, the page you requested, and the personally identifiable information you entered.</h4>
+                    <h4 class="mb-3" style="text-align: justify;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        We use information for three general purposes: to customize the advertising and content you see, to fulfill your requests for certain products and services, and to contact you in case of emergency.</h4>
                 </div>
             </div>
             <div class="col-md-6 col-lg-6">
-                <div class="single_works wow fadeInUp">
-                    <h2 class="pt-4 mb-3" style="color: #0098c7;"><b>Changes to this Privacy Policy</b></h2>
-                    <p>We may amend this policy from time to time. If we make any substantial changes in the way we use your personal information we will notify you by posting a prominent announcement on our pages.</p>
-                    <p>You agree that such posting will constitute sufficient and adequate notice to you of the changes. You agree that your continued use of our products or services after such amendment will constitute your acceptance of an agreement to be bound by the modified policy.</p>
-                    </div>
+                <div class="single_works wow fadeInUp pl-5">
+                    <h1 class="mt-4 mb-5" style="color: #0098c7;">Changes to this Privacy Policy</h1>
+                    <h4 class="mb-3" style="text-align: justify;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        We may amend this policy from time to time. If we make any substantial changes in the way we use your personal information we will notify you by posting a prominent announcement on our pages.
+                    </h4>
+                    <h4 class="mb-3" style="text-align: justify;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        You agree that such posting will constitute sufficient and adequate notice to you of the changes. You agree that your continued use of our products or services after such amendment will constitute your acceptance of an agreement to be bound by the modified policy.
+                    </h4>
+                </div>
             </div>
         </div>
+    </div>
+</section>
